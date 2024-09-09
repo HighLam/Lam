316b19b6a3dc43b534f668a54d6655e6337114ac
High Lam <hhighlam2004@gmail.com>
2024-09-09 21:20:58 +0700

    add ok

---
+++ src/main/java/com/example/gitdemo/Git1/ngu.java
@@ -0,0 +1,10 @@
+package com.example.gitdemo.Git1;
+
+public class ngu {
+    private int id;
+    private String name;
+    public ngu(int id, String name) {
+        this.id = id;
+        this.name = name;git
+    }
+}
